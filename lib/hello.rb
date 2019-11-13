36876c82376e14c2d5a905407a494d989ac986ff
Monkey Mendel <stephanie.mendel26@gmail.com>
2019-11-13 23:43:59 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,10 +1,10 @@
-#def hello_t(array)
- # i = 0
-#  while i < array.length
-   # yield array[1]
-  #  i = i + 1
- # end
-#end
+def hello_t(array)
+  i = 0
+  while i < array.length
+    yield array[1]
+    i = i + 1
+  end
+end
 
 # call your method here!
 
