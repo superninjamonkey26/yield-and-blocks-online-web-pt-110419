95d91a114f1b77b5fd3da499591c54d241fc9c87
Monkey Mendel <stephanie.mendel26@gmail.com>
2019-11-14 00:59:05 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -10,11 +10,3 @@ def hello_t(array)
     puts "Hey! No block was given!"
   end
 end
-
-# call your method here!
-
-#hello_t(["Tim", "Tom", "Jim"]) do |name|
- # if name.start_with?("T")
-#    puts "Hi, #{name}"
- # end
-#end
